435805154a563fc96cb1352c61e8d418b4265e59
dominhdang148 <myclone235@gmail.com>
2025-03-13 11:08:57 +0700

    Refactor Admin's endpoints

--- blog-app-be/src/main/java/com/dominhdang/blog_app/features/tag/controller/TagAdminController.java
+++ blog-app-be/src/main/java/com/dominhdang/blog_app/features/tag/controller/TagAdminController.java
@@ -1,13 +1,52 @@
 package com.dominhdang.blog_app.features.tag.controller;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dominhdang.blog_app.features.tag.dto.TagDetailDto;
+import com.dominhdang.blog_app.features.tag.dto.TagFormDto;
+import com.dominhdang.blog_app.features.tag.service.TagService;
+import com.dominhdang.blog_app.models.ApiResponse;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(path = "api/admin/tag")
 @Tag(name = "Tag Admin Endpoints", description = "Tag's endpoints for administrator access")
 public class TagAdminController {
+    @Autowired
+    private TagService tagService;
 
+    @PostMapping
+    public ResponseEntity<ApiResponse<TagDetailDto>> addTag(@Valid @RequestBody TagFormDto tag) {
+
+        ApiResponse<TagDetailDto> response = this.tagService.saveTag(tag);
+        return ResponseEntity.status(response.getStatus()).body(response);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<TagDetailDto>> updateTag(@PathVariable UUID id, @RequestBody TagFormDto tag) {
+        ApiResponse<TagDetailDto> response = this.tagService.saveTag(id, tag);
+
+        return ResponseEntity
+                .status(response.getStatus())
+                .body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<?>> deleteTag(@PathVariable UUID id) {
+        ApiResponse<?> response = this.tagService.deleteTag(id);
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
 }
--- blog-app-be/src/main/java/com/dominhdang/blog_app/features/tag/controller/TagController.java
+++ blog-app-be/src/main/java/com/dominhdang/blog_app/features/tag/controller/TagController.java
@@ -5,25 +5,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dominhdang.blog_app.features.tag.dto.TagDetailDto;
-import com.dominhdang.blog_app.features.tag.dto.TagFormDto;
 import com.dominhdang.blog_app.features.tag.dto.TagItemDto;
 import com.dominhdang.blog_app.features.tag.service.TagService;
 import com.dominhdang.blog_app.models.ApiResponse;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping(name = "Tags Endpoints", path = "api/tag")
@@ -45,29 +39,6 @@ public class TagController {
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<TagDetailDto>> getDetailTag(@PathVariable UUID id) {
         ApiResponse<TagDetailDto> response = this.tagService.getById(id);
-        return ResponseEntity.status(response.getStatus()).body(response);
-    }
-
-    @PostMapping
-    public ResponseEntity<ApiResponse<TagDetailDto>> addTag(@Valid @RequestBody TagFormDto tag) {
-
-        ApiResponse<TagDetailDto> response = this.tagService.saveTag(tag);
-        return ResponseEntity.status(response.getStatus()).body(response);
-
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<TagDetailDto>> updateTag(@PathVariable UUID id, @RequestBody TagFormDto tag) {
-        ApiResponse<TagDetailDto> response = this.tagService.saveTag(id, tag);
-
-        return ResponseEntity
-                .status(response.getStatus())
-                .body(response);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<?>> deleteTag(@PathVariable UUID id) {
-        ApiResponse<?> response = this.tagService.deleteTag(id);
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
